8f6fcc889fa718f550d0175b85783c80db2bd8ca
Martin Vancl <tux.martin@gmail.com>
2014-11-20 17:54:36 +0100

    -Java verze ctecky (komplikovane, a nespolehlive)

--- pc/server.py
+++ pc/server.py
@@ -19,7 +19,7 @@ signal.signal(signal.SIGINT, signal_handler)
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
 	#port='/dev/ttyUSB0',
-	port='/dev/ttyS0',
+	port='/dev/ttyUSB0',
 	baudrate=9600,
 	#writeTimeout = 2,
 	parity=serial.PARITY_NONE,
@@ -27,48 +27,43 @@ ser = serial.Serial(
 	bytesize=serial.EIGHTBITS
 )
 
+try: # pokud uz byl port ortevreny, zkusim ho zavrit
+	if ser.isOpen():
+		ser.close()
+except Exception, e:
+    print "error open serial port: " + str(e)
+    exit()
+
 try: 
     ser.open()
 except Exception, e:
     print "error open serial port: " + str(e)
     exit()
 
-
 if ser.isOpen():
 	try:
 		ser.flushInput() #flush input buffer, discarding all its contents
 		ser.flushOutput() #flush output buffer, aborting current output 
 
-    #ser.write("ABC")
-		#time.sleep(0.5)  #give the serial port sometime to receive the data
-	while True:
-		nacteno = ser.readline()
-		if ( len(nacteno) ) < 5:
-			print "Arduino poslalo neplatna data!"
-		else:
-			userAgent = "Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0";
-			uvod = "ID+va%C5%A1%C3%AD+karty+je"
-			cti = uvod + nacteno
-			# "wget -U \"" + userAgent + "\" -O - \"http://translate.google.com/translate_tts?ie=UTF-8&tl=cs&q=" + cti + "\" | madplay -"
-			prikaz = "wget -U \"" + userAgent + "\" -O - \"http://translate.google.com/translate_tts?ie=UTF-8&tl=cs&q=" + cti + "\" | madplay -"
-			print prikaz
-			call(["/bin/sh", "-c", prikaz])
-			
-"""
-			String[] cmd = {
-							"/bin/sh",
-							"-c",
-							"wget -U \"" + userAgent + "\" -O - \"http://translate.google.com/translate_tts?ie=UTF-8&tl=cs&q=" + cti + "\" | madplay -"
-							//"wget -U Mozilla -O - \"http://translate.google.com/translate_tts?ie=UTF-8&tl=cs&q=" + cti + "\" | madplay -"
-							};
-"""
-    
+		#ser.write("ABC")
+			#time.sleep(0.5)  #give the serial port sometime to receive the data
+		while True:
+			nacteno = ser.readline()
+			ser.flushInput() #flush input buffer, discarding all its contents
+			if ( len(nacteno) ) < 5:
+				print "Arduino poslalo neplatna data!"
+			else:
+				userAgent = "Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0";
+				uvod = "ID+va%C5%A1%C3%AD+karty+je"
+				cti = uvod + nacteno
+				# "wget -U \"" + userAgent + "\" -O - \"http://translate.google.com/translate_tts?ie=UTF-8&tl=cs&q=" + cti + "\" | madplay -"
+				prikaz = "wget -U \"" + userAgent + "\" -O - \"http://translate.google.com/translate_tts?ie=UTF-8&tl=cs&q=" + cti + "\" | madplay -"
+				print prikaz
+				call(["/bin/sh", "-c", prikaz])
 
-    except Exception, e1:
-
-    print "error communicating...: " + str(e1)
+	except Exception, e1:
+		print "error communicating...: " + str(e1)
 
 else:
-
-    print "cannot open serial port "
+	print "cannot open serial port "
 
